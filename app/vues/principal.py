b8759f56e24a3d4404cef04b81d6e0345b83fe6c
sofiajpp <sofiajpp@rocketmail.com>
2016-01-05 16:52:19 +0100

    Update principal.py

--- app/vues/principal.py
+++ app/vues/principal.py
@@ -28,20 +28,20 @@ def index():
         arrivee = {'position': geographie.geocoder(localisation_arr)}
 
         adresse_dep = modeles.Adresse(
-            adresse=form.adresse_dep.data,
-            numero=form.numero_dep.data,
-            cp=form.cp_dep.data,
-            ville=form.ville_dep.data,
-            position='POINT({0} {1})'.format(
+            adresse = form.adresse_dep.data,
+            numero = form.numero_dep.data,
+            cp = form.cp_dep.data,
+            ville = form.ville_dep.data,
+            position = 'POINT({0} {1})'.format(
                 depart['position']['lat'], depart['position']['lon'])
         )
 
         adresse_arr = modeles.Adresse(
-            adresse=form.adresse_arr.data,
-            numero=form.numero_arr.data,
-            cp=form.cp_arr.data,
-            ville=form.ville_arr.data,
-            position='POINT({0} {1})'.format(
+            adresse = form.adresse_arr.data,
+            numero = form.numero_arr.data,
+            cp = form.cp_arr.data,
+            ville = form.ville_arr.data,
+            position = 'POINT({0} {1})'.format(
                 arrivee['position']['lat'], arrivee['position']['lon'])
         )
         # Ajouter l'adresse à la BD
@@ -52,15 +52,15 @@ def index():
 
         flash('La demande de réservation a été prise en compte.', 'positive')
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, titre='Réserver un taxi')
+    return render_template('index.html', form = form, titre = 'Réserver un taxi')
 
 
 @app.route('/carte')
 def carte():
-    return render_template('carte.html', titre='Carte')
+    return render_template('carte.html', titre = 'Carte')
 
 
-@app.route('/rafraichir_carte', methods=['POST'])
+@app.route('/rafraichir_carte', methods = ['POST'])
 def rafraichir_carte():
     lat = random.uniform(48.8434100, 48.8634100)
     lon = random.uniform(2.3388000, 2.3588000)
@@ -69,45 +69,45 @@ def rafraichir_carte():
 
 @app.route('/tarifs')
 def tarifs():
-    return render_template('tarifs.html', titre='Tarifs')
+    return render_template('tarifs.html', titre = 'Tarifs')
 
 
 @app.route('/informations')
 def informations():
-    return render_template('informations.html', titre='Informations')
+    return render_template('informations.html', titre = 'Informations')
 
 
 @app.route('/FAQ')
 def faq():
     faq_data = utile.lire_json('app/static/data/faq.json')
-    return render_template('faq.html', titre='FAQ', faq_data=faq_data)
+    return render_template('faq.html', titre = 'FAQ', faq_data = faq_data)
 
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', titre='Contact')
+    return render_template('contact.html', titre = 'Contact')
 
 
 @app.route('/api')
 def api():
-    return render_template('api.html', titre='API')
+    return render_template('api.html', titre = 'API')
 
 @app.route('/G7')
 def G7():
-    return render_template('G7.html', titre='G7')
+    return render_template('G7.html', titre = 'G7')
     
 @app.route('/Attribution')
 def Attribution():
-    return render_template('Attribution.html', titre='Attribution')
+    return render_template('Attribution.html', titre = 'Attribution')
     
 @app.route('/Statut')
 def Statut():
-    return render_template('Statut.html', titre='Statut')
+    return render_template('Statut.html', titre = 'Statut')
     
 @app.route('/Navigation')
 def Navigation():
-    return render_template('conducteur/navigation.html', titre='Navigation')
+    return render_template('conducteur/navigation.html', titre = 'Navigation')
     
 @app.route('/CarteDesBeauxGosses')
 def CarteDesBeauxGosses():
-    return render_template('CarteDesBeauxGosses.html', titre='CarteDesBeauxGosses')
+    return render_template('CarteDesBeauxGosses.html', titre = 'CarteDesBeauxGosses')
